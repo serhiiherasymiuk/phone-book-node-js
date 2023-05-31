0b0f02eeb8fc8303f61fcd0366a8880760cdceb7
SerhiiHerasymiuk <@DESKTOP-8DFR7S5>
2023-05-31 20:32:28 +0300

    Test of MongoDB

--- src/express.js
+++ src/express.js
@@ -1,6 +1,33 @@
 const express = require("express"),
   app = express();
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://sgerasimuk07:6r7MqMoANySlVR5Y@cluster0.jr0y54t.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 const fs = require("fs");
 
 const address = "127.0.0.1";
